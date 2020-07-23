171c45883c5572a162d79dda818d0e5b1060139c
永野 朗夫 <nagano@me.com>
2020-07-23 10:01:36 +0900

    modify internalLink.js (optimize adding ID function

--- _javascript/internalLink.js
+++ _javascript/internalLink.js
@@ -2,41 +2,79 @@
 
 // 内部リンク
 export default function internalLink() {
-  let $navLink = "";
+  let $navLink = '';
 
-  const createIndex = () => {
-    const indexWrap = document.querySelector(".internal-link"); //H2タグの前に挿入したdivタグ
-    if (indexWrap) {
-      const postContent = document.querySelector(".md-content"); //記事本文が書かれているラッパー
-      const hTags = postContent.querySelectorAll("h2, h3"); //記事内のH2とH3タグを全て取得
-      const hTags_include_h4 = postContent.querySelectorAll("h2, h3, h4");
+  function createIndex() {
+    console.log('createIndex')
+    const tocContainer = document.querySelector('.internal-link');
+    if (tocContainer) {
+      const postContent = document.querySelector('.md-content');
+      const headings = postContent.querySelectorAll('h2, h3, h4');
+      const outline = [];
+      let currentNode = outline;
+      for (let i = 0; i < headings.length; i++) {
+        const currentHeading = headings[i];
+        // make outline
+        switch (currentHeading.tagName) {
+          case 'H2':
+            currentNode = [];
+            outline.push(currentHeading, currentNode);
+            break;
+          case 'H3':
+            currentNode.push(currentHeading);
+            break;
+        }
+        // add ID
+        const namedAnchor = currentHeading.querySelector('a[name]');
+        if (namedAnchor) {
+          // 見出しにname属性持ちのアンカーがあれば、name属性をIDに 
+          currentHeading.setAttribute('id', namedAnchor.name);
+        } else if (!currentHeading.getAttribute('id')) {
+          // そうでなく、かつ見出しにIDが付与されていなければ、見出し内容をIDに
+          // （ただしどうやらJekyllは自動的にそれをやっているっぽい。だとしたら不要
+          // * encodeURIComponent は不要？
+          currentHeading.setAttribute('id', currentHeading.textContent.replace(/[\/\s]/g, '-'));
+        }
+        // add anchor
+        if (currentHeading.tagName === 'H2' || currentHeading.tagName === 'H3') {
+          const anchor = document.createElement('a');
+          currentHeading.appendChild(anchor);
+          anchor.setAttribute('href', '#' + currentHeading.id);
+          anchor.classList.add('link-icon');
+        }
+      }
+      console.log(outline);
+
+
+
+      const hTags = postContent.querySelectorAll('h2, h3');
+      const hTagsIncludeH4 = postContent.querySelectorAll('h2, h3, h4');
       if (hTags.length > 0) {
-        let indexList = document.createElement("ul");
-        let listSrc = "";
-        let subListSrc = ""; //h3タグを取得しておくための変数
+        const indexList = document.createElement('ul');
+        let listSrc = '';
+        let subListSrc = ''; //h3タグを取得しておくための変数
 
         for (let i = 0; i < hTags.length; i++) {
           let theHeading = hTags[i];
-          let theHeadingIncludeH4 = hTags_include_h4[i]
-          let id = "";
-          if ( $(theHeadingIncludeH4).children('a').attr("name") ){//既にidがあれば
-            id = $(theHeadingIncludeH4).children('a').attr("name");//既存のidを使う
+          let theHeadingIncludeH4 = hTagsIncludeH4[i]
+          let id = '';
+          if ( $(theHeadingIncludeH4).children('a').attr('name') ){//既にidがあれば
+            id = $(theHeadingIncludeH4).children('a').attr('name');//既存のidを使う
           }else{
-            id = $(theHeadingIncludeH4).text().replace(/ |:/g, "_").replace(/\(|\)/g, "");//不要な箇所を除去
+            id = $(theHeadingIncludeH4).text().replace(/ |:/g, '_').replace(/\(|\)/g, '');//不要な箇所を除去
             id = jQuery.trim(id);//不要な箇所を除去
           }
           id = encodeURIComponent(id);//idをエンコード
-          $(theHeadingIncludeH4).attr("id", id);//リンクで飛べるようにIDをつける
-          //ここのid名がhref属性に指定されているaタグをh2、もしくはh3の後に挿入
-          const a = document.createElement('a');
-          theHeading.appendChild(a);
-          a.setAttribute("href","#" + id);
-          a.classList.add("link-icon");
-          if (theHeading.tagName === "H2") {
-            if (subListSrc !== "") {
+          //$(theHeadingIncludeH4).attr("id", id);//リンクで飛べるようにIDをつける
+          //const a = document.createElement('a');
+          //theHeading.appendChild(a);
+          //a.setAttribute("href","#" + id);
+          //a.classList.add("link-icon");
+          if (theHeading.tagName === 'H2') {
+            if (subListSrc !== '') {
               //h3リストが生成されていれば
-              listSrc += "<ul>" + subListSrc + "</ul>";
-              subListSrc = "";
+              listSrc += '<ul>' + subListSrc + '</ul>';
+              subListSrc = '';
             }
             listSrc +=
               '</li><li><a href="#' +
@@ -54,15 +92,15 @@ export default function internalLink() {
           }
         }
 
-        if (subListSrc !== "") {
+        if (subListSrc !== '') {
           //最後のリストがh3だった場合
           listSrc += "<ul>" + subListSrc + "</ul></li>";
         } else {
           listSrc += "</li>";
         }
         indexList.innerHTML = listSrc;
-        //indexWrap.appendChild(indexList);
-        indexWrap.innerHTML = indexList.outerHTML;
+        //tocContainer.appendChild(indexList);
+        tocContainer.innerHTML = indexList.outerHTML;
       }
     }
     $navLink = $("#TableOfContents li a");
@@ -71,6 +109,7 @@ export default function internalLink() {
   // 各コンテンツのページ上部からの開始位置と終了位置を配列に格納しておく
   let contentsArr = new Array();
   const createContentsArr = () => {
+    console.log('createContentsArr')
     for (let i = 0; i < $navLink.length; i++) {
       // コンテンツのIDを取得
       let targetContents = $navLink.eq(i).attr("href");
@@ -90,6 +129,7 @@ export default function internalLink() {
 
   // 現在地をチェックする
   function currentCheck() {
+    console.log('currentCheck')
     // 現在のスクロール位置を取得
     let windowScrolltop = $(window).scrollTop();
     for (let i = 0; i < contentsArr.length; i++) {
@@ -118,7 +158,7 @@ export default function internalLink() {
   // ナビゲーションクリック時の動作：スムーズスクロール・アドレスバーにID付与
   const smoothScroll = () => {
     $navLink.on("click", function () {
-      history.replaceState("", "", this.href);
+      history.replaceState('', '', this.href);
       $("html,body").animate(
         {
           scrollTop: $($(this).attr("href")).offset().top - 24,
@@ -129,11 +169,14 @@ export default function internalLink() {
     });
   };
 
+  console.log('before of promise1')
   let promise = new Promise((resolve, reject) => {
     createIndex();
+    console.log('before of resolve')
     resolve();
   });
 
+  console.log('before of promise2')
   promise
     .then(() => {
       createContentsArr();
