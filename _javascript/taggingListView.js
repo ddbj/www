ee884bdbf4f09e8654cb1582473bef4324223c3e
永野 朗夫 <nagano@me.com>
2020-11-23 20:01:36 +0900

    change news page's layout, and make news index page (in progress..

--- _javascript/taggingListView.js
+++ _javascript/taggingListView.js
@@ -2,64 +2,133 @@
 
 // サービス一覧
 export default function taggingListView() {
+
   const taggingListView = document.querySelector('.tagging-list-view');
   if (taggingListView) {
-    // タグの収集
-    const categoryTags = {}, affiliationTags = {}, tagLabels = {};
-    const taggingItems = taggingListView.querySelectorAll('.taggingitem');
-    for (const item of taggingItems) {
-      const tags = item.querySelectorAll('.tags > .tag-view');
-      for (const tag of tags) {
-        let array;
-        if (tag.classList.contains('-reverse')) {
-          // 所属
-          array = affiliationTags;
-        } else {
-          // カテゴリ
-          array = categoryTags;
-        }
-        if (!array[tag.dataset.tag]) {
-          array[tag.dataset.tag] = [];
-          tagLabels[tag.dataset.tag] = tag.textContent;
-        }
-        array[tag.dataset.tag].push(item);
-      }
-    }
-    // ファセット検索用ビューの生成
-    let html = '';
-    const facetSearchTags = document.querySelector('.facet-search > .tags');
-    for (const tags of [categoryTags, affiliationTags]) {
-      for (const tag in tags) {
-        html += `<li class="tag-view${affiliationTags[tag] === undefined ? '' : ' -reverse'}" data-tag="${tag}">${tagLabels[tag]}<span class="count">${tags[tag].length}</span></li>`;
-      }
-    }
-    facetSearchTags.innerHTML = html;
-    // インタラクション
-    const URIParameters = $.deparam(window.location.search.substr(1)).tags;
-    const selectedTags = URIParameters ? URIParameters : [];
-    const tagViews = facetSearchTags.querySelectorAll('.tag-view');
-    updateTag();
-    // インタラクション
-    tagViews.forEach(tagView => tagView.addEventListener('click', () => {
-      const tag = tagView.dataset.tag;
-      // 選択中のタグに追加・削除
-      if (selectedTags.indexOf(tag) === -1) {
-        selectedTags.push(tag);
-      } else {
-        selectedTags.splice(selectedTags.indexOf(tag), 1);
-      }
-      // URLパラメータにセット
-      window.history.pushState(selectedTags, '', `${ window.location.origin }${ window.location.pathname }?${ $.param({tags: selectedTags}) }`);
-      updateTag();
-    }))
+    let taggingItems, selectedTags, tagViews;
+    const facetSearch = document.querySelector('.facet-search');
 
-    // アイテムのインタラクション
-    taggingListView.querySelectorAll('.bottom.-collapsed').forEach(elm => {
-      const collapsedbutton = elm.querySelector('.collapsedbutton');
-      collapsedbutton.addEventListener('click', () => {
-        elm.classList.toggle('-opened');
-      });
-    })
+    setupTabMenu();
+    setupKeywordTag();
+    setupYearTag();
+    setupFacetSearch();
+
+    /**
+     * タブ切り替え
+     */
+    function setupTabMenu() {
+      const tabMenuView = facetSearch.querySelector(':scope > .tab-menu-view');
+      if (tabMenuView) {
+        const tabMenuItems = tabMenuView.querySelectorAll(':scope > .tabmenucontainer > li');
+        const tabbedContents = tabMenuView.querySelectorAll(':scope > .tabbedcontent');
+        tabMenuItems.forEach(item => {
+          item.target = tabMenuView.querySelector(`:scope > .tabbedcontent[data-tab="${item.dataset.tab}"]`);
+          item.addEventListener('click', () => {
+            // 表示状態の切り替え
+            tabMenuItems.forEach(item => item.classList.remove('-current'));
+            item.classList.add('-current');
+            tabbedContents.forEach(tabbedContent => tabbedContent.classList.remove('-current'));
+            item.target.classList.add('-current');
+          });
+        });
+      }
+    }
+
+    /**
+     * ファセット検索の準備
+     */
+    function setupKeywordTag() {
+      const categoryTags = {}, affiliationTags = {}, tagLabels = {};
+      taggingItems = taggingListView.querySelectorAll('.taggingitem');
+      // タグの収集
+      for (const item of taggingItems) {
+        const tags = item.querySelectorAll('.tags > .tag-view');
+        for (const tag of tags) {
+          let array;
+          if (tag.classList.contains('-reverse')) {
+            // 所属
+            array = affiliationTags;
+          } else {
+            // カテゴリ
+            array = categoryTags;
+          }
+          if (!array[tag.dataset.tag]) {
+            array[tag.dataset.tag] = [];
+            tagLabels[tag.dataset.tag] = tag.textContent;
+          }
+          array[tag.dataset.tag].push(item);
+        }
+      }
+      // ファセット検索用ビューの生成
+      let html = '';
+      const facetSearchTags = facetSearch.querySelector(':scope .tags');
+      for (const tags of [categoryTags, affiliationTags]) {
+        for (const tag in tags) {
+          html += `<li class="tag-view${affiliationTags[tag] === undefined ? '' : ' -reverse'}" data-tag="${tag}">${tagLabels[tag]}<span class="count">${tags[tag].length}</span></li>`;
+        }
+      }
+      facetSearchTags.innerHTML = html;
+
+      // アイテムのインタラクション
+      taggingListView.querySelectorAll('.bottom.-collapsed').forEach(elm => {
+        const collapsedbutton = elm.querySelector('.collapsedbutton');
+        collapsedbutton.addEventListener('click', () => {
+          elm.classList.toggle('-opened');
+        });
+      })
+
+    }
+
+    function setupYearTag() {
+      if (facetSearch.dataset.facetSearchYear === 'true') {
+        const yearTags = {};
+        // タグの収集
+        for (const item of taggingItems) {
+          const datetime = item.querySelector('.tags > .date > time').getAttribute('datetime');
+          const year = new Date(datetime).getFullYear();
+          if (!yearTags[year]) {
+            yearTags[year] = [];
+          }
+          yearTags[year].push(item);
+        }
+        // ファセット検索用ビューの生成
+        let html = '';
+        const facetSearchTags = facetSearch.querySelector(':scope .tags[data-tab="year"]');
+        console.log(facetSearchTags)
+        for (const tag in yearTags) {
+          html += `<li class="tag-view" data-tag="${tag}">${tag}<span class="count">${yearTags[tag].length}</span></li>`;
+        }
+        facetSearchTags.innerHTML = html;
+      }
+    }
+
+    function setupFacetSearch() {
+      // インタラクション
+      const URIParameters = $.deparam(window.location.search.substr(1)).tags;
+      console.log(window.location.search.substr(1));
+      console.log(URIParameters);
+      selectedTags = URIParameters ? URIParameters : [];
+      const facetSearchTags = facetSearch.querySelector(':scope .tags');
+      console.log(facetSearchTags);
+      tagViews = facetSearchTags.querySelectorAll('.tag-view');
+      const path = window.location.origin + window.location.pathname;
+      updateTag();
+      // インタラクション
+      tagViews.forEach(tagView => tagView.addEventListener('click', () => {
+        const tag = tagView.dataset.tag;
+        // 選択中のタグに追加・削除
+        if (selectedTags.indexOf(tag) === -1) {
+          selectedTags.push(tag);
+        } else {
+          selectedTags.splice(selectedTags.indexOf(tag), 1);
+        }
+        // URLパラメータにセット
+        const parameters = selectedTags.length ? `?${ $.param({tags: selectedTags}) }` : '';
+        window.history.pushState(selectedTags, '', `${ path }${ parameters }`);
+        updateTag();
+      }))
+
+    }
 
     // アイテムの表示更新
     function updateTag() {
